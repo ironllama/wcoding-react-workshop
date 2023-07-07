f17b87c8005b3fe4cc42e564cfa585cdcc2a0d0d
Alex <alexoh@wcoding.com>
2023-07-07 20:48:26 +0900

    Added vite.

---
+++ weather-app-vite/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+
+import WeatherInput from './components/WeatherInput';
+import WeatherOutput from './components/WeatherOutput';
+
+import './App.css';
+
+function App() {
+  const [apiSymbol, setApiSymbol] = useState("");
+  const [apiDesc, setApiDesc] = useState("");
+  const [apiTemp, setApiTemp] = useState("");
+  const [apiCity, setApiCity] = useState("");
+
+  useEffect(function () {
+    getWeather("Seoul");
+    // console.log("Dude");
+  }, []);
+
+  function getWeather(city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=3f36a008bba9d213f7a6e6101f6588c1`
+    )
+      .then((res) => res.json())
+      .then((resObj) => {
+        // console.log(resObj);
+        const newCity = `${resObj.city.name} ${resObj.city.country}`;
+        const newTemp = resObj.list[0].main.temp;
+        const { description, icon } = resObj.list[0].weather[0];
+
+        setApiCity(newCity);
+        setApiTemp(newTemp);
+        setApiSymbol(icon);
+        setApiDesc(description);
+      });
+  }
+
+  function onInput(text) {
+    // console.log("Use API to get weather for", text);
+    getWeather(text);
+  }
+
+  return (
+    <div>
+      <WeatherInput onSubmit={onInput} />
+      <WeatherOutput
+        city={apiCity}
+        symbol={apiSymbol}
+        desc={apiDesc}
+        temp={apiTemp}
+      />
+    </div>
+  );
+}
+
+export default App
---
+++ weather-app-vite/src/components/WeatherInput.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+import './WeatherInput.css';
+
+export default function WeatherInput({ onSubmit }) {
+  const [typing, setTyping] = useState("");
+
+  function onType(evt) {
+    setTyping(evt.target.value);
+  }
+
+  function finishedTyping() {
+    onSubmit(typing);
+  }
+
+  return (
+    <div className="weatherInput">
+      <input type="text" className="" onChange={onType} />
+      <button className="resultButton" onClick={finishedTyping}>Result</button>
+    </div>
+  );
+}
